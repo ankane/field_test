19477e4dc0e529b150ab9a23238e6cb9671eab97
Andrew Kane <acekane1@gmail.com>
2016-12-17 14:15:45 -0800

    Added experiments page

--- app/controllers/field_test/experiments_controller.rb
+++ app/controllers/field_test/experiments_controller.rb
@@ -9,5 +9,13 @@ module FieldTest
     def index
       @active_experiments, @completed_experiments = FieldTest::Experiment.all.sort_by(&:id).partition { |e| e.active? }
     end
+
+    def show
+      begin
+        @experiment = FieldTest::Experiment.find(params[:id])
+      rescue FieldTest::ExperimentNotFound
+        raise ActionController::RoutingError, "Experiment not found"
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 FieldTest::Engine.routes.draw do
+  resources :experiments, only: [:show]
   root "experiments#index"
 end
