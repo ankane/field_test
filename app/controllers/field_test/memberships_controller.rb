8dc24edce6250161873ea41a4e7fb3d616e3ac0a
Andrew Kane <acekane1@gmail.com>
2016-12-17 17:39:42 -0800

    Added ability to update variants from dashboard

---
+++ app/controllers/field_test/memberships_controller.rb
@@ -0,0 +1,15 @@
+module FieldTest
+  class MembershipsController < BaseController
+    def update
+      membership = FieldTest::Membership.find(params[:id])
+      membership.update_attributes(membership_params)
+      redirect_to participant_path(membership.participant)
+    end
+
+    private
+
+      def membership_params
+        params.require(:membership).permit(:variant)
+      end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 FieldTest::Engine.routes.draw do
   resources :experiments, only: [:show]
+  resources :memberships, only: [:update]
   get "participants/:id", to: "participants#show", constraints: {id: /.+/}, as: :participant
   root "experiments#index"
 end
