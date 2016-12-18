c581f7885aff71680e5cdaca1ecd0175d0a33b2f
Andrew Kane <acekane1@gmail.com>
2016-12-17 17:10:13 -0800

    More speed!

--- lib/field_test/experiment.rb
+++ lib/field_test/experiment.rb
@@ -186,6 +186,19 @@ module FieldTest
         # for performance
         logbeta_ac_bc = Math.logbeta(alpha_c, beta_c)
         abc = beta_a + beta_b + beta_c
+        log_bb_j = []
+        logbeta_j_bb = []
+        0.upto(alpha_b - 1) do |j|
+          log_bb_j[j] = Math.log(beta_b + j)
+          logbeta_j_bb[j] = Math.logbeta(1 + j, beta_b)
+        end
+
+        logbeta_ac_i_j = []
+        0.upto(alpha_a - 1) do |i|
+          0.upto(alpha_b - 1) do |j|
+            logbeta_ac_i_j[i + j] = Math.logbeta(alpha_c + i + j, abc)
+          end
+        end
 
         0.upto(alpha_a - 1) do |i|
           # for performance
@@ -193,9 +206,9 @@ module FieldTest
           logbeta_i_ba = Math.logbeta(1 + i, beta_a)
 
           0.upto(alpha_b - 1) do |j|
-            total += Math.exp(Math.logbeta(alpha_c + i + j, abc) -
-              log_ba_i - Math.log(beta_b + j) -
-              logbeta_i_ba - Math.logbeta(1 + j, beta_b) -
+            total += Math.exp(logbeta_ac_i_j[i + j] -
+              log_ba_i - log_bb_j[j] -
+              logbeta_i_ba - logbeta_j_bb[j] -
               logbeta_ac_bc)
           end
         end
