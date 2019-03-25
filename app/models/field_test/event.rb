6d8dc1e661aa16a71025d46dab2c434d5f74274f
Kyle Schmolze <kyletns@gmail.com>
2019-03-25 14:45:14 -0700

    make event names unique per membership_id

--- app/models/field_test/event.rb
+++ app/models/field_test/event.rb
@@ -3,6 +3,6 @@ module FieldTest
     self.table_name = "field_test_events"
 
     belongs_to :field_test_membership, class_name: "FieldTest::Membership"
-    validates :name, presence: true
+    validates :name, presence: true, uniqueness: { scope: :field_test_membership_id }
   end
 end
