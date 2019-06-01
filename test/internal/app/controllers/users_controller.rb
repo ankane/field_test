3d5f2882e52de6f928522ccce0e9d589239350a4
Andrew Kane <andrew@chartkick.com>
2019-06-01 16:06:08 -0700

    WIP [skip ci]

--- test/internal/app/controllers/users_controller.rb
+++ test/internal/app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ActionController::Base
   def index
+    @button_color = field_test(:button_color)
+    field_test_converted(:button_color)
+    @experiments = field_test_experiments
   end
 end
