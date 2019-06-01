19bd63f103ef057bce6c46dee01a3e9dc21f1289
Andrew Kane <andrew@chartkick.com>
2019-06-01 15:02:02 -0700

    Verbose

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,4 +9,8 @@ Combustion.initialize! :all do
   if config.active_record.sqlite3.respond_to?(:represent_boolean_as_integer)
     config.active_record.sqlite3.represent_boolean_as_integer = true
   end
+
+  logger = ActiveSupport::Logger.new(STDOUT)
+  config.active_record.logger = logger if ENV["VERBOSE"]
+  config.action_mailer.logger = logger if ENV["VERBOSE"]
 end
