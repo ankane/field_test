9ee9a1a62a3da180954b0598dd1ed9200090fdcb
Andrew Kane <andrew@chartkick.com>
2019-06-02 10:29:01 -0700

    Added params test

--- test/mailer_test.rb
+++ test/mailer_test.rb
@@ -33,4 +33,17 @@ class MailerTest < Minitest::Test
       UserMailer.welcome.deliver_now
     end
   end
+
+  def test_user_param
+    skip unless Rails.version >= "5.1"
+
+    user = User.create!
+    UserMailer.with(user: user).welcome.deliver_now
+
+    membership = FieldTest::Membership.last
+    assert_equal 1, FieldTest::Membership.count
+    assert membership.converted
+    assert_equal "User", membership.participant_type
+    assert_equal user.id.to_s, membership.participant_id
+  end
 end
