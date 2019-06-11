970790d8e98d1c4a22d52184602d3674576da988
Andrew Kane <andrew@chartkick.com>
2019-06-11 04:05:15 -0700

    Cleaned up generator [skip ci]

--- lib/generators/field_test/install_generator.rb
+++ lib/generators/field_test/install_generator.rb
@@ -1,23 +1,10 @@
-require "rails/generators"
-require "rails/generators/migration"
-require "active_record"
 require "rails/generators/active_record"
 
 module FieldTest
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      include Rails::Generators::Migration
-      source_root File.expand_path("../templates", __FILE__)
-
-      # Implement the required interface for Rails::Generators::Migration.
-      def self.next_migration_number(dirname) #:nodoc:
-        next_migration_number = current_migration_number(dirname) + 1
-        if ::ActiveRecord::Base.timestamped_migrations
-          [Time.now.utc.strftime("%Y%m%d%H%M%S"), "%.14d" % next_migration_number].max
-        else
-          "%.3d" % next_migration_number
-        end
-      end
+      include ActiveRecord::Generators::Migration
+      source_root File.join(__dir__, "templates")
 
       def copy_migration
         migration_template "memberships.rb", "db/migrate/create_field_test_memberships.rb", migration_version: migration_version
