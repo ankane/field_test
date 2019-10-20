2da76a2942899995ac348c6a7ddb3c966fc26c31
Andrew Kane <andrew@chartkick.com>
2019-10-20 13:48:40 -0700

    Better logger pattern [skip ci]

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,13 +4,14 @@ Bundler.require(:default)
 require "minitest/autorun"
 require "minitest/pride"
 
+logger = ActiveSupport::Logger.new(ENV["VERBOSE"] ? STDOUT : nil)
+
 Combustion.path = "test/internal"
 Combustion.initialize! :active_record, :action_controller, :action_mailer do
   if ActiveRecord::VERSION::MAJOR < 6 && config.active_record.sqlite3.respond_to?(:represent_boolean_as_integer)
     config.active_record.sqlite3.represent_boolean_as_integer = true
   end
 
-  logger = ActiveSupport::Logger.new(STDOUT)
-  config.active_record.logger = logger if ENV["VERBOSE"]
-  config.action_mailer.logger = logger if ENV["VERBOSE"]
+  config.active_record.logger = logger
+  config.action_mailer.logger = logger
 end
