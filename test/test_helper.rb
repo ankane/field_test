bd4582163283a1eccd625b4c8f1999b4ff180be1
Andrew Kane <andrew@chartkick.com>
2019-10-20 13:47:52 -0700

    Fixed tests for Sprockets 4

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,7 +5,7 @@ require "minitest/autorun"
 require "minitest/pride"
 
 Combustion.path = "test/internal"
-Combustion.initialize! :all do
+Combustion.initialize! :active_record, :action_controller, :action_mailer do
   if ActiveRecord::VERSION::MAJOR < 6 && config.active_record.sqlite3.respond_to?(:represent_boolean_as_integer)
     config.active_record.sqlite3.represent_boolean_as_integer = true
   end
