8fa536ce3c40bdbd8bd2022480e6e0dcef2b885e
Andrew Kane <andrew@ankane.org>
2021-10-15 14:30:38 -0700

    Use bayestest

--- ext/field_test/ext.cpp
+++ ext/field_test/ext.cpp
@@ -1,12 +1,12 @@
 #include <rice/rice.hpp>
-#include "bayesian_ab.hpp"
+#include "bayestest.hpp"
 
 extern "C"
 void Init_ext() {
   auto rb_mFieldTest = Rice::define_module("FieldTest");
 
   Rice::define_class_under(rb_mFieldTest, "Calculations")
-    .define_singleton_function("prob_b_beats_a", &bayesian_ab::prob_b_beats_a)
-    .define_singleton_function("prob_c_beats_a_and_b", &bayesian_ab::prob_c_beats_a_and_b)
-    .define_singleton_function("prob_d_beats_a_and_b_and_c", &bayesian_ab::prob_d_beats_a_and_b_and_c);
+    .define_singleton_function("prob_b_beats_a", &bayestest::prob_b_beats_a)
+    .define_singleton_function("prob_c_beats_a_and_b", &bayestest::prob_c_beats_ab)
+    .define_singleton_function("prob_d_beats_a_and_b_and_c", &bayestest::prob_d_beats_abc);
 }
