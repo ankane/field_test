44cc246a7ebcbdb2d4d27fd0555a2c5db27e0ade
Andrew Kane <andrew@ankane.org>
2021-10-16 14:24:28 -0700

    Moved prob_winning tests to separate file

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -15,3 +15,13 @@ Combustion.initialize! :active_record, :action_controller, :action_mailer do
   config.active_record.logger = logger
   config.action_mailer.logger = logger
 end
+
+class Minitest::Test
+  def set_variant(experiment, variant, participant_id)
+    FieldTest::Membership.create!(
+      experiment: experiment.id,
+      variant: variant,
+      participant_id: participant_id
+    )
+  end
+end
