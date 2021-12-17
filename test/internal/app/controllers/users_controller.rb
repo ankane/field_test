18f1f1cf66f14e2dcab0b3edaf48a33326f672df
Dávid Falusi <10588147+falusi94@users.noreply.github.com>
2021-12-17 02:27:01 +0100

    Fix exclude option in #field_test helper method (#36)

--- test/internal/app/controllers/users_controller.rb
+++ test/internal/app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ActionController::Base
   def index
-    @button_color = field_test(:button_color)
+    @button_color = field_test(:button_color, exclude: params[:exclude])
     field_test_converted(:button_color)
     @experiments = field_test_experiments
   end
