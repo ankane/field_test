54cb3e620f2546cd9fd237c0d085463e923f0a2a
Andrew Kane <andrew@ankane.org>
2023-12-24 14:48:45 -0500

    Added config.load_defaults to test setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,9 +8,7 @@ logger = ActiveSupport::Logger.new(ENV["VERBOSE"] ? STDOUT : nil)
 
 Combustion.path = "test/internal"
 Combustion.initialize! :active_record, :action_controller, :action_mailer do
-  if ActiveRecord::VERSION::MAJOR < 6 && config.active_record.sqlite3.respond_to?(:represent_boolean_as_integer)
-    config.active_record.sqlite3.represent_boolean_as_integer = true
-  end
+  config.load_defaults Rails::VERSION::STRING.to_f
 
   config.active_record.logger = logger
   config.action_mailer.logger = logger
