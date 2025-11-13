57b84b327085dbf9fd91b6925525ae011ef6d2cc
Andrew Kane <andrew@ankane.org>
2025-11-13 01:42:19 -0800

    Moved protect_from_forgery after authentication

--- app/controllers/field_test/base_controller.rb
+++ app/controllers/field_test/base_controller.rb
@@ -1,9 +1,9 @@
 module FieldTest
   class BaseController < ActionController::Base
-    layout "field_test/application"
+    http_basic_authenticate_with name: ENV["FIELD_TEST_USERNAME"], password: ENV["FIELD_TEST_PASSWORD"] if ENV["FIELD_TEST_PASSWORD"]
 
     protect_from_forgery with: :exception
 
-    http_basic_authenticate_with name: ENV["FIELD_TEST_USERNAME"], password: ENV["FIELD_TEST_PASSWORD"] if ENV["FIELD_TEST_PASSWORD"]
+    layout "field_test/application"
   end
 end
